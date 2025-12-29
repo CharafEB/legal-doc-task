307d2cc459270f56d5cddff78cb072d8a5ed1b06
charaf007tmp <76937494+CharafEB@users.noreply.github.com>
2025-12-29 12:15:57 +0100

    fix the last api for the cros error

--- view/app/components/card.tsx
+++ view/app/components/card.tsx
@@ -15,7 +15,7 @@ type DocumentCardProps = Document & {
 
 export const DocumentCard = (data: DocumentCardProps) => (
   <a
-    href={`http://localhost:8080/document?documentTitle=${data.title}`}
+    href={`https://legal-doc-task.onrender.com/document?documentTitle=${data.title}`}
     target="_blank"
     rel="noreferrer"
     className="group-hover:text-[var(--primary)] transition-colors"
@@ -90,4 +90,3 @@ export const DocumentCard = (data: DocumentCardProps) => (
     </div>
   </a>
 );
-
